08e8016693af77a21842e5b93a0af32b123384e0
MansourWolou <Wolou0444@gmail.com>
2021-07-06 03:04:11 +0200

    making display something

--- src/com/company/Main/Main.java
+++ src/com/company/Main/Main.java
@@ -1,5 +1,7 @@
 package com.company.Main;
 
+import com.company.View.View;
+
 import javax.swing.*;
 
 public class Main {
@@ -9,7 +11,8 @@ public class Main {
                 new Runnable() {
                     @Override
                     public void run() {
-
+                        View view = new View("testoo",520,520);
+                        view.setVisible(true);
                     }
                 }
         );
--- src/com/company/View/View.java
+++ src/com/company/View/View.java
@@ -1,4 +1,28 @@
 package com.company.View;
 
-public class View {
+import javax.swing.*;
+import java.awt.*;
+
+public class View extends JFrame {
+
+    public View(String name, int w, int h){
+        super(name);
+        this.build(w,h);
+    }
+    //setting the window
+    private void build(int w, int h) {
+        this.setPreferredSize(new Dimension(w,h));
+        this.setLocationRelativeTo((Component) null);
+        this.setResizable(false);
+        this.setDefaultCloseOperation(2);
+        this.pack();
+        this.buildContentPane();
+    }
+
+    private void buildContentPane() {
+        JPanel panel = new JPanel();
+        panel.setLayout((LayoutManager)null);
+        this.getContentPane().add(panel);
+        panel.setBackground(Color.decode("#151515"));
+    }
 }
