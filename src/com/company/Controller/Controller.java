8e890f6a2fb798409e59f6aeb851acb338fd90fa
MansourWolou <Wolou0444@gmail.com>
2021-07-10 14:34:54 +0200

    update request done

--- src/com/company/Controller/Controller.java
+++ src/com/company/Controller/Controller.java
@@ -1,10 +1,9 @@
 package com.company.Controller;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
 
 public class Controller {
+    private String displayedText;
 
     public Controller(){
 
@@ -18,8 +17,23 @@ public class Controller {
     //delete the actual display
     public void deleteContentDisplay(){};
     // update the actuel text display
-    public void updateContentDisplay(){};
-    public  static void connect(){
+    public void updateContentDisplay(String updateTxt){
+      //  System.out.println(updateTxt.toCharArray().toString()+'ur');
+        String req = "UPDATE quote\n" +
+                "SET textImput = " + "\'" + updateTxt + "\'" +"\n" +
+                "WHERE textImput= " +"\'" + displayedText+ "\'";
+        System.out.println(req);
+        try {
+            Connection conn = this.connect();
+            Statement stmt  = conn.createStatement();
+            int rs    = stmt.executeUpdate(req);
+            System.out.println(rs);
+        }catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+
+    };
+    private Connection connect(){
         Connection conn = null;
         try {
             String url = "jdbc:sqlite:/home/woloumanour/strategieOblique/src/com/company/text.db";
@@ -27,20 +41,34 @@ public class Controller {
             System.out.println("Connection to SQLite has been established.");
         }catch (SQLException e){
             System.out.println(e.getMessage());
-        }finally {
-            try {
-                if (conn != null){
-                    conn.close();
-                }
-            }catch (SQLException ex){
-                System.out.println(ex.getMessage());
-            }
+        }
+        /*
+        https://stackoverflow.com/questions/9561832/what-if-i-dont-close-the-database-connection-in-python-sqlite
+        this website said that we are not obliged to close the database
+         */
+        return conn;
+    }
+    public void getRandomText(){
+        String sql = "SELECT *\n" +
+                "FROM quote\n" +
+                "ORDER BY random()\n" +
+                "LIMIT 1;";
+        try {
+            Connection conn = this.connect();
+            Statement stmt  = conn.createStatement();
+            ResultSet rs    = stmt.executeQuery(sql);
+            System.out.println(rs.getString("textImput"));
+            displayedText = rs.getString("textImput");
+            conn.close();
+        }catch (SQLException e) {
+            System.out.println(e.getMessage());
         }
     }
-
     public static void main(String[] args) {
 
         Controller control = new Controller();
-        control.connect();
+        control.getRandomText();
+        control.updateContentDisplay("coucou");
+
     }
 }
